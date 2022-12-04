650fed90e68e3a81f66d312b9adebb0d6cf0ef2f
Charles Moll <charliemoll@email.arizona.edu>
2022-12-03 17:15:04 -0700

    Added JdatePicker and Updated Cal GUI

---
+++ eclipse-workspace/CalendarApp/src/driver/GUI_Driver.java
@@ -0,0 +1,10 @@
+package driver;
+import menu.main.*;
+import menu.calendar.*;
+public class GUI_Driver {
+
+	public static void main(String[] args) {
+		CalendarGUI G = new CalendarGUI();
+		
+	}
+}
--- eclipse-workspace/CalendarApp/src/menu/calendar/CalendarGUI.java
+++ eclipse-workspace/CalendarApp/src/menu/calendar/CalendarGUI.java
@@ -1,5 +1,31 @@
 package menu.calendar;
 
-public class CalendarGUI {
+import java.util.Properties;
 
+import javax.swing.JFrame;
+
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
+
+public class CalendarGUI {
+	UtilDateModel model;
+	JDatePanelImpl datePanel;
+	JDatePickerImpl datePicker;
+	
+	public CalendarGUI() {
+		JFrame f = new JFrame();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(300,300);
+		f.setVisible(true);
+		model = new UtilDateModel();
+		Properties p = new Properties();
+		p.put("text.today", "Today");
+		p.put("text.month", "Month");
+		p.put("text.year", "Year");
+		JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
+		JDatePickerImpl datePicker = new JDatePickerImpl(datePanel,null);
+		f.add(datePicker);
+	}
+	
 }
