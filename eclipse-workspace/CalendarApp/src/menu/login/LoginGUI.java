5192c9c3f9ef3e5fd7e3669d5d43418deac811bc
Richard Gin <richardgin@email.com>
2022-12-05 23:39:58 -0700

    Updated LoginGUI and MainGUI

--- eclipse-workspace/CalendarApp/src/menu/login/LoginGUI.java
+++ eclipse-workspace/CalendarApp/src/menu/login/LoginGUI.java
@@ -13,32 +13,36 @@ public class LoginGUI extends JFrame {
 	private JPanel panel;
 	private JButton registerButton;
 	private JButton loginButton;
+	private JButton exitButton;
 	private DB db;
 	
-	public LoginGUI(DB db) {
+	// LoginGUI Constructor that creates the Login Menu and calls BuildGUI to build it
+	public LoginGUI() {
 		super("Login Menu");
-		this.db = db;
+		this.db = new DB("Data.db");
 		
-		setSize(850, 300);
-		setLayout(new FlowLayout(FlowLayout.LEFT));
+		setSize(1000, 300);
+		setLayout(new FlowLayout(FlowLayout.CENTER));
 		setBackground(Color.white);
 		add(new JLabel("<HTML><center>Welcome to the ECE373 Calendar Project" + "<BR>This is the Login menu. "
-						+ "Please select Register if you would like to create a new user, and Login if you would"
-						+ " like to login to an existing account</center><HTML>"));
+						+ "Please select Register if you would like to create a new user, Login if you would"
+						+ " like to login to an existing account, or Exit to exit the program</center><HTML>"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		buildGUI();
 		setVisible(true);
 	}
 	
+	
+	// Method to build the GUI for the Login Menu
+	// Creates Register, Login, and Exit buttons
 	public void buildGUI() {
 		panel = new JPanel();
 		panel.setBounds(600, 600, 600, 600);
 		panel.setBackground(Color.white);
-		
-		
+
 		registerButton = new JButton("Register");
 		registerButton.setBounds(500, 1000, 80, 30);
-		registerButton.setBackground(Color.red);
+		registerButton.setBackground(Color.orange);
 		registerButton.addActionListener(new ActionPerformed());
 		
 		loginButton = new JButton("Login");
@@ -46,29 +50,41 @@ public class LoginGUI extends JFrame {
 		loginButton.setBackground(Color.green);
 		loginButton.addActionListener(new ActionPerformed());
 		
+		exitButton = new JButton("Exit");
+		exitButton.setBounds(1500, 1000, 80, 30);
+		exitButton.setBackground(Color.red);
+		exitButton.addActionListener(new ActionPerformed());
+		
 		panel.add(registerButton);
 		panel.add(loginButton);
+		panel.add(exitButton);
 		
 		add(panel);
 	}
 	
-	
+	// ActionPerformed class to implement button actions
 	public class ActionPerformed implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
-			JButton source = (JButton) (e.getSource());
-			
-			if(source.equals(registerButton)) {
+			JButton source = (JButton) (e.getSource());		// Gets user input if button is pressed
+			if(source.equals(registerButton)) {		// Calls handleRegister if Register is pressed
 				handleRegister();
 			}
-			else if(source.equals(loginButton)) {
+			else if(source.equals(loginButton)) {	// Calls handleLogin if Login is pressed
 				handleLogin();
+			}
+			else if(source.equals(exitButton)) {	// Calls handleExit if Exit is pressed
+				handleExit();
 			}
 		}
 		
-		// FIXME Debug back-end interaction with database
+		// FIXME Debug back-end interaction with database, not adding Users
+		// FIXME If user presses close, then the program displays an error message
+		/* This method handles the Register button. If it is pressed User input will be taken for
+		 * a username, password, and confirmPassword. It will perform error checking on the input
+		 * and if no input errors are found, a User will be created and added to the database
+		 * */
 		private void handleRegister() {
 			// Creating Panel Components
 			JTextField username = new JTextField(5);
@@ -76,17 +92,17 @@ public class LoginGUI extends JFrame {
 			JTextField passwordConfirm = new JTextField(5);
 			
 			// Getting Panel Inputs and Displaying Panel
-			JPanel panel = new JPanel(new GridLayout(3, 1));
-			panel.add(new JLabel("Enter Username: "));
-			panel.add(username);
-			panel.add(Box.createHorizontalStrut(20));
-			panel.add(new JLabel("Enter Password: "));
-			panel.add(password);
-			panel.add(Box.createHorizontalStrut(20));
-			panel.add(new JLabel("Confirm Password: "));
-			panel.add(passwordConfirm);
-			panel.add(Box.createHorizontalStrut(20));
-			JOptionPane.showMessageDialog(null, panel);
+			JPanel panelR = new JPanel(new GridLayout(3, 1));
+			panelR.add(new JLabel("Enter Username: "));
+			panelR.add(username);
+			panelR.add(Box.createHorizontalStrut(20));
+			panelR.add(new JLabel("Enter Password: "));
+			panelR.add(password);
+			panelR.add(Box.createHorizontalStrut(20));
+			panelR.add(new JLabel("Confirm Password: "));
+			panelR.add(passwordConfirm);
+			panelR.add(Box.createHorizontalStrut(20));
+			JOptionPane.showMessageDialog(null, panelR);
 			// Getting Panel Inputs as Strings
 			String u = username.getText();
 			String p = password.getText();
@@ -97,6 +113,10 @@ public class LoginGUI extends JFrame {
 			JOptionPane.showMessageDialog(null, result, "Register Menu", JOptionPane.PLAIN_MESSAGE);
 		}
 		
+		/* Helper method to handle the Register button. It takes in user input and performs error checking
+		 * to determine if the input is valid. If it is, a Login is created and the User is added to the
+		 * database
+		 *  */
 		private String createUser(String username, String password, String passwordConfirm) {
 			// If the password and passwordConfirm do not match, the user is not created
 			if(username.equals("") || password.equals("") || passwordConfirm.equals("")) {
@@ -114,6 +134,7 @@ public class LoginGUI extends JFrame {
 			return "This user already exists, please try 'Login' instead of 'Register'";
 		}
 		
+		/* Helper method to add User to the database */
 		private void addUserToDatabase(Login login) {
 			ArrayList<Object> rowData = new ArrayList<>();
 			rowData.add(login.getUserID());
@@ -124,26 +145,53 @@ public class LoginGUI extends JFrame {
 		}
 		
 		//FIXME Add back-end interaction with database
+		/* This method handles the Login button. If it is pressed then user input is received and
+		 * verified. If it is valid then the User is logged in and moves to the Main Menu 
+		 * */
 		private void handleLogin() {
 			// Creating Panel Components
 			JTextField username = new JTextField(5);
 			JTextField password = new JTextField(5);
 			
 			// Getting Panel Inputs and Displaying Panel
-			JPanel panel = new JPanel(new GridLayout(2, 1));
-			panel.add(new JLabel("Enter Username: "));
-			panel.add(username);
-			panel.add(Box.createHorizontalStrut(20));
-			panel.add(new JLabel("Enter Password: "));
-			panel.add(password);
-			panel.add(Box.createHorizontalStrut(20));
-			JOptionPane.showMessageDialog(null,  panel);
+			JPanel panelL = new JPanel(new GridLayout(2, 1));
+			panelL.add(new JLabel("Enter Username: "));
+			panelL.add(username);
+			panelL.add(Box.createHorizontalStrut(20));
+			panelL.add(new JLabel("Enter Password: "));
+			panelL.add(password);
+			panelL.add(Box.createHorizontalStrut(20));
+			JOptionPane.showMessageDialog(null,  panelL);
 			// Getting Panel Inputs as Strings
 			String u = username.getText();
 			String p = password.getText();
+			
+			if(checkInput(u, p)) {		// Checking if User input is valid
+				Login login = new Login(db, "Login", u, p);		// Checking User Login information
+				if(login.getUserID() == -1) {
+					JOptionPane.showMessageDialog(null, "Username/Password does not exist", "Login Menu", JOptionPane.PLAIN_MESSAGE);
+				}
+				else {
+					// FIXME OPEN MAIN MENU
+				}
+			}
 		}
 		
+		private boolean checkInput(String u, String p) {
+			if(u.equals("") || p.equals("")) {
+				JOptionPane.showMessageDialog(null, "Invalid Input: Input cannot be empty/blank", "Login Menu", JOptionPane.PLAIN_MESSAGE);
+				return false;
+			}
+			return true;
+		}
 		
+		/* This method handles the Exit button. If it is pressed then the database is closed
+		 * and the program is ended.
+		 * */
+		private void handleExit() {
+			db.close();
+			System.exit(0);
+		}
 	}
 	
 	
--- eclipse-workspace/CalendarApp/src/menu/main/MainGUI.java
+++ eclipse-workspace/CalendarApp/src/menu/main/MainGUI.java
@@ -1,5 +1,82 @@
 package menu.main;
 
-public class MainGUI {
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import db.setup.DB;
+import menu.calendar.CalendarGUI;
+import menu.edit.EditGUI;
+
+public class MainGUI extends JFrame {
+	private JPanel panel;
+	private JButton editButton;
+	private JButton displayButton;
+	private DB db;
+	private int userID;
+	
+	public MainGUI(DB db, int userID) {
+		super("Main Menu");
+		this.db = db;
+		this.userID = userID;
+		
+		setSize(500, 300);
+		setLayout(new FlowLayout(FlowLayout.CENTER));
+		setBackground(Color.white);
+		add(new JLabel("<HTML><center>Welcome to the Main Menu" + "<BR>Please select Edit to edit your calendar, or"
+						+ " Display to display your calendar</center><HTML>"));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		buildGUI();
+		setVisible(true);
+	}
+	
+	public void buildGUI() {
+		panel = new JPanel();
+		panel.setBounds(600, 600, 600, 600);
+		panel.setBackground(Color.white);
+		
+		editButton = new JButton("Edit");
+		editButton.setBounds(500, 1000, 80, 30);
+		editButton.setBackground(Color.red);
+		editButton.addActionListener(new ActionPerformed());
+		
+		displayButton = new JButton("Display");
+		displayButton.setBounds(1000, 1000, 80, 30);
+		displayButton.setBackground(Color.green);
+		displayButton.addActionListener(new ActionPerformed());
+		
+		panel.add(editButton);
+		panel.add(displayButton);
+		
+		add(panel);
+	}
+	
+	// ActionPerformed class to implement button actions
+	public class ActionPerformed implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JButton source = (JButton) (e.getSource());		// Gets user input if button is pressed
+			if(source.equals(editButton)) {		// Calls handleRegister if Register is pressed
+				handleEdit();
+			}
+			else if(source.equals(displayButton)) {	// Calls handleLogin if Login is pressed
+				handleDisplay();
+			}
+		}
+		
+		private void handleEdit() {
+			EditGUI edit = new EditGUI();
+		}
+		
+		private void handleDisplay() {
+			CalendarGUI calendar = new CalendarGUI();
+		}
+	}
 }
