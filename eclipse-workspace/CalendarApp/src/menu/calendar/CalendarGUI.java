691d9710555a38fab58dff24303ccab80ac76748
Charles Moll <charliemoll@email.arizona.edu>
2022-12-06 15:43:56 -0700

    Update Calendar GUI

--- eclipse-workspace/CalendarApp/src/menu/calendar/CalendarGUI.java
+++ eclipse-workspace/CalendarApp/src/menu/calendar/CalendarGUI.java
@@ -1,15 +1,26 @@
 package menu.calendar;
 
+import datatype.*;
+import db.*;
+
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.Rectangle;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Properties;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
+import javax.swing.JTable;
+import javax.swing.JFormattedTextField.AbstractFormatter;
 
 import org.jdatepicker.impl.JDatePanelImpl;
 import org.jdatepicker.impl.JDatePickerImpl;
@@ -18,22 +29,24 @@ import org.jdatepicker.impl.UtilDateModel;
 
 public class CalendarGUI extends JFrame{
 	
+	private int DateSelected;
+	private UtilDateModel model = new UtilDateModel();
+	JFrame frame;
 	public CalendarGUI() {
-		
+		this.frame = new JFrame("Calendar");
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame.setSize(400,300);
+		this.frame.setLayout(new BorderLayout());
+		this.frame.setLocationRelativeTo(null);
 	}
 	/**
 	 * 
 	 */
-	public void GUI() {
-		
-		JFrame frame = new JFrame("Calendar");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400,300);
-		frame.setLayout(new BorderLayout());
+	public void CalendarPage() {
 		
 		//----------------------------------------
-        UtilDateModel model = new UtilDateModel();
-   
+       
+       
 		Properties p = new Properties();
 		p.put("text.today", "Today");
 		p.put("text.month", "Month");
@@ -41,24 +54,25 @@ public class CalendarGUI extends JFrame{
 		
 		JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
 		datePanel.setSize(450, 450);
+		
 		JDatePickerImpl datePicker = new JDatePickerImpl(datePanel,new DateLabelFormatter());
 		
 		datePicker.setBounds(100, 100, 800, 800); //doesn't work
 		//model.setSelected(true);
-		datePicker.setVisible(true);
 		
+		datePicker.setVisible(true);
+	
 		
 		//-------------------------------------
 		
 		JPanel topPanel = new JPanel();
-		JPanel middlePanel = new JPanel();
 		JPanel bottomPanel = new JPanel();
 		
 		JButton select = new JButton("View Date");
+		select.addMouseListener(new ViewEventsListener());
 		select.setBounds(EXIT_ON_CLOSE, ABORT, 100, 100);
 		topPanel.add(datePicker);
 		topPanel.setBounds(100, 100, 450, 450);
-		middlePanel.setBounds(EXIT_ON_CLOSE, ABORT, 100, 100);
 		bottomPanel.add(select);
 		
 		JPanel mainPanel = new JPanel();
@@ -69,14 +83,81 @@ public class CalendarGUI extends JFrame{
 		
 		
 		//---------------------------------------------
-//		pane.setDividerSize(0);
-//		pane2.setDividerSize(0);
+
 		frame.add(mainPanel);
-		
 		
 		//--------------------------------------
 		
 		frame.setVisible(true);
+	}
+	
+	public void VeiwDay(datatype.Date D) {
+		//JTable here
+		System.out.println(D);
+		
+		JTable table = new JTable();
+		
+		frame.add(table);
+	}
+	
+	private class ViewEventsListener implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			//display new menu 
+			System.out.println("Button Clicked");
+			//close current page
+			frame.getContentPane().removeAll();
+			frame.revalidate();
+			frame.repaint();
+			//open next page (Month starts at 0 for some reason)
+			VeiwDay(new datatype.Date(model.getMonth() + 1 ,model.getDay(),model.getYear()));
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	public class DateLabelFormatter extends AbstractFormatter {
+
+	    private String datePattern = "MM/dd/yyyy";
+	    private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
+
+	    @Override
+	    public Object stringToValue(String text) throws ParseException {
+	        return dateFormatter.parseObject(text);
+	    }
+
+	    @Override
+	    public String valueToString(Object value) throws ParseException {
+	        if (value != null) {
+	            Calendar cal = (Calendar) value;
+	            return dateFormatter.format(cal.getTime());
+	        }
+
+	        return "";
+	    }
 	}
 	
 }
