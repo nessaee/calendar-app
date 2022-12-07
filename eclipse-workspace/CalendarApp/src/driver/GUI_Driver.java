fc31e4b516cf72ba2219cff2f98e57857db5e9a1
Snoop <ameernessaee9536@gmail.com>
2022-12-07 01:01:22 -0700

    Update GUI_Driver.java

--- eclipse-workspace/CalendarApp/src/driver/GUI_Driver.java
+++ eclipse-workspace/CalendarApp/src/driver/GUI_Driver.java
@@ -1,10 +1,15 @@
 package driver;
 import menu.main.*;
+import db.setup.DB;
 import menu.calendar.*;
+import menu.login.LoginGUI;
 public class GUI_Driver {
 
 	public static void main(String[] args) {
-		CalendarGUI G = new CalendarGUI();
+		DB db = new DB("Data.db");
+        db.setConnection(db.connect());
+        LoginGUI g = new LoginGUI(db);
+		
 		
 	}
 }
